abda6cf780d7849be0601f5f374173565cc4a045
Umair Rathore <contactusr22@gmail.com>
2023-03-29 01:41:18 +0500

    user list on dashboard, and removed extra things

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,6 +11,9 @@ class DashboardController extends Controller
     //
     public function index()
     {
-        return view('backend.dashboard');
+        $show = User::all();
+        return view('backend.dashboard', compact('show'));
+
     }
+
 }
