0468aa455efff74c99f612f8d93adcb0b6ecb852
Umair Rathore <contactusr22@gmail.com>
2023-03-31 12:14:30 +0500

    Profile Image in job section home page

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,45 +2,47 @@
 @section('title', 'Home')
 @section('content')
 
-      <div class="Search-section">
+    <div class="Search-section">
         <div class="container">
             <div class="row">
-                  <div class="col-lg-10 col-md-5 col-12">
+                <div class="col-lg-10 col-md-5 col-12">
                     <div class="form-group mb-0">
                         <input class="search-1" type="text" placeholder="Keywords (e.g. Location,Job Title, Position...)">
-                      </div>
+                    </div>
                 </div>
                 <div class="col-lg-2 col-md-3 col-12 mt-15">
-                      <a href="{{route('browse_jobs')}}"><button class="srch-btn" type="submit">Search Now</button></a>
+                    <a href="{{route('browse_jobs')}}">
+                        <button class="srch-btn" type="submit">Search Now</button>
+                    </a>
                 </div>
             </div>
-          </div>
+        </div>
     </div>
     <div class="banner-slider">
-          <div class="owl-carousel bnnr-owl owl-theme">
-              @foreach($cities as $city)
-            <div class="item">
+        <div class="owl-carousel bnnr-owl owl-theme">
+            @foreach($cities as $city)
+                <div class="item">
 
-                <div class="featured-cities">
-                <a href="{{route('browse_freelancers')}}">
-                        <div class="feature-img">
-                            <img src="{{asset($city->c_image)}}" style="height:300px" alt="">
-                              <div class="overly-bg"></div>
-                        </div>
-                    </a>
-                    <a href="{{route('browse_freelancers')}}">
-                        <div class="featured-text">
-                            <div class="city-title">{{$city->city}}</div>
-                              <ins>125 Freelancers</ins>
-                        </div>
-                    </a>
-                  </div>
-            </div>
-              @endforeach
-         </div>
+                    <div class="featured-cities">
+                        <a href="{{route('browse_freelancers')}}">
+                            <div class="feature-img">
+                                <img src="{{asset($city->c_image)}}" style="height:300px" alt="">
+                                <div class="overly-bg"></div>
+                            </div>
+                        </a>
+                        <a href="{{route('browse_freelancers')}}">
+                            <div class="featured-text">
+                                <div class="city-title">{{$city->city}}</div>
+                                <ins>125 Freelancers</ins>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
     <div class="achivements">
-         <div class="container">
+        <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-12">
                     <div class="achive-text">3M Registered Members</div>
@@ -50,12 +52,14 @@
                 </div>
                 <div class="col-lg-2 col-md-6 col-12">
 
-     <div class="achive-text">1.2K Best Companies</div>
+                    <div class="achive-text">1.2K Best Companies</div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-12">
                     <ul class="post-buttons">
                         <li>
-                            <a href="{{route('post_a_job')}}"><button class="add-job">Post a Job</button></a>
+                            <a href="{{route('post_a_job')}}">
+                                <button class="add-job">Post a Job</button>
+                            </a>
                         </li>
                     </ul>
                 </div>
@@ -77,17 +81,17 @@
                 <div class="col-md-12 col-12">
                     <div class="job-categories mt-30">
                         <div class="row no-gutters">
-                @foreach($professions as $profession)
-                            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                                <div class="p-category">
-                                    <a href="{{route('browse_jobs')}}" title="">
-                                        <img src="{{$profession->p_image}}" alt="">
-                                        <span>{{$profession->profession}}</span>
-                                        <p>150 Jobs</p>
-                                    </a>
+                            @foreach($professions as $profession)
+                                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                                    <div class="p-category">
+                                        <a href="{{route('browse_jobs')}}" title="">
+                                            <img src="{{$profession->p_image}}" alt="">
+                                            <span>{{$profession->profession}}</span>
+                                            <p>150 Jobs</p>
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
-                    @endforeach
+                            @endforeach
                         </div>
                     </div>
                 </div>
@@ -111,37 +115,44 @@
 
                     <div class="lts-jobs-slider">
                         <div class="owl-carousel jobs-owl owl-theme">
-                @foreach($jobs as $job)
-                            <div class="item">
-                                <div class="job-item">
-                                    <div class="job-top-dt">
-                                        <div class="job-left-dt">
-                                            <img src="images/homepage/latest-jobs/img-1.jpg" alt="">
-                                            <div class="job-ut-dts">
-                                                <a href="#"><h4>{{$job->first_name.' '.$job->last_name}}</h4></a>
-                                                <span><i class="fas fa-map-marker-alt"></i> {{$job->location}}</span>
+                            @foreach($jobs as $job)
+                                <div class="item">
+                                    <div class="job-item">
+                                        <div class="job-top-dt">
+                                            <div class="job-left-dt">
+                                                @if($job->profile_image)
+
+                                                    <img src="{{asset($job->profile_image)}}" alt="">
+
+                                                @else
+                                                    <img src="{{asset('images/homepage/latest-jobs/img-1.jpg')}}" alt="">
+
+                                                @endif
+                                                <div class="job-ut-dts">
+                                                    <a href="#"><h4>{{$job->first_name.' '.$job->last_name}}</h4></a>
+                                                    <span><i class="fas fa-map-marker-alt"></i> {{$job->location}}</span>
+                                                </div>
+                                            </div>
+                                            <div class="job-right-dt">
+                                                <div class="job-price">${{$job->budget}}</div>
+                                                <div class="job-fp">{{$job->time_of_day}}</div>
                                             </div>
                                         </div>
-                                        <div class="job-right-dt">
-                                            <div class="job-price">${{$job->budget}}</div>
-                                            <div class="job-fp">{{$job->time_of_day}}</div>
+                                        <div class="job-des-dt">
+                                            <h4>{{$job->profession}}</h4>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus pulvinar dolor nec...</p>
+                                        </div>
+                                        <div class="job-buttons">
+                                            <ul class="link-btn">
+                                                <li><a href="#" class="link-j1" title="Apply Now">APPLY NOW</a></li>
+                                                <li><a href="{{route('job_single_view')}}" class="link-j1" title="View Job">View Job</a></li>
+                                                <li class="bkd-pm">
+                                                    <button class="bookmark1" title="bookmark"><i class="fas fa-heart"></i></button>
+                                                </li>
+                                            </ul>
                                         </div>
                                     </div>
-                                    <div class="job-des-dt">
-                                        <h4>{{$job->profession}}</h4>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus pulvinar dolor nec...</p>
-                                    </div>
-                                    <div class="job-buttons">
-                                        <ul class="link-btn">
-                                            <li><a href="#" class="link-j1" title="Apply Now">APPLY NOW</a></li>
-                                            <li><a href="{{route('job_single_view')}}" class="link-j1" title="View Job">View Job</a></li>
-                                            <li class="bkd-pm">
-                                                <button class="bookmark1" title="bookmark"><i class="fas fa-heart"></i></button>
-                                            </li>
-                                        </ul>
-                                    </div>
                                 </div>
-                            </div>
                             @endforeach
                         </div>
                         <div class="text-center">
@@ -219,48 +230,55 @@
                     <div class="lts-jobs-slider">
                         <div class="owl-carousel jobs-owl owl-theme">
                             @foreach($users as $user)
-                            <div class="item">
-                                <div class="job-item">
-                                    <div class="job-top-dt1 text-center">
-                                        <div class="job-center-dt">
-                                            <img src="images/homepage/candidates/img-1.jpg" alt="">
-                                            <div class="job-urs-dts">
-                                                <a href="#"><h4>{{$user->first_name.' '.$user->last_name}}</h4></a>
-                                                <span>{{$user->profession}}</span>
+                                <div class="item">
+                                    <div class="job-item">
+                                        <div class="job-top-dt1 text-center">
+                                            <div class="job-center-dt">
+                                                @if($user->profile_image)
+
+                                                    <img src="{{asset($user->profile_image)}}" alt="">
+
+                                                @else
+                                                    <img src="{{asset('images/homepage/latest-jobs/img-1.jpg')}}" alt="">
+
+                                                @endif
+                                                <div class="job-urs-dts">
+                                                    <a href="#"><h4>{{$user->first_name.' '.$user->last_name}}</h4></a>
+                                                    <span>{{$user->profession}}</span>
+                                                </div>
+                                            </div>
+                                            <div class="job-price hire-price">{{$user->pay_rate}}</div>
+                                        </div>
+                                        <div class="rating-location">
+                                            <div class="left-rating">
+                                                <div class="rtitle">Rating</div>
+                                                <div class="star">
+                                                    <i class="fas fa-star"></i>
+                                                    <i class="fas fa-star"></i>
+                                                    <i class="fas fa-star"></i>
+                                                    <i class="fas fa-star"></i>
+                                                    <i class="fas fa-star"></i>
+                                                    <span>4.9</span>
+                                                </div>
+                                            </div>
+                                            <div class="right-location">
+                                                <div class="text-left">
+                                                    <div class="rtitle">Location</div>
+                                                    <span><i class="fas fa-map-marker-alt"></i> {{$user->location}}</span>
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="job-price hire-price">{{$user->pay_rate}}</div>
-                                    </div>
-                                    <div class="rating-location">
-                                        <div class="left-rating">
-                                            <div class="rtitle">Rating</div>
-                                            <div class="star">
-                                                <i class="fas fa-star"></i>
-                                                <i class="fas fa-star"></i>
-                                                <i class="fas fa-star"></i>
-                                                <i class="fas fa-star"></i>
-                                                <i class="fas fa-star"></i>
-                                                <span>4.9</span>
-                                            </div>
+                                        <div class="job-buttons">
+                                            <ul class="link-btn">
+                                                <li><a href="{{route('other_freelancer_profile',[$user->id])}}" class="link-j1" title="View Profile">View Profile</a></li>
+                                                <li><a href="#" class="link-j1" title="Hire Me">Hire Me</a></li>
+                                                <li class="bkd-pm">
+                                                    <button class="bookmark1" title="bookmark"><i class="fas fa-heart"></i></button>
+                                                </li>
+                                            </ul>
                                         </div>
-                                        <div class="right-location">
-                                            <div class="text-left">
-                                                <div class="rtitle">Location</div>
-                                                <span><i class="fas fa-map-marker-alt"></i> {{$user->location}}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="job-buttons">
-                                        <ul class="link-btn">
-                                            <li><a href="{{route('other_freelancer_profile')}}" class="link-j1" title="View Profile">View Profile</a></li>
-                                            <li><a href="#" class="link-j1" title="Hire Me">Hire Me</a></li>
-                                            <li class="bkd-pm">
-                                                <button class="bookmark1" title="bookmark"><i class="fas fa-heart"></i></button>
-                                            </li>
-                                        </ul>
                                     </div>
                                 </div>
-                            </div>
                             @endforeach
                         </div>
                         <div class="text-center">
